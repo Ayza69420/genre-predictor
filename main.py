8ca9f49c18631232a45ed4b6937090c76a609461
Ayza <87999011+Ayza69420@users.noreply.github.com>
2022-06-26 14:34:24 +0300

    Optimization and AI improvement

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
 t_d, d = [], []
-classifying = {"male": {}, "female": {}}
 
-train_data_path = "./train_data.csv"
-test_data_path = "./test_data.csv"
+classifying = {"male": {}, "female": {}}
+occurrences = {"male": {}, "female": {}}
+genres = set()
+
+train_data_path = "C:/Users/ayza6/Documents/train_data.csv"
+test_data_path = "C:/Users/ayza6/Documents/test_data.csv"
 
 train_data = pd.read_csv(train_data_path)
 test_data = pd.read_csv(test_data_path)
@@ -12,40 +15,37 @@ test_data = pd.read_csv(test_data_path)
 for row in train_data.iterrows():
     age, gender, genre= row[1].values
     
-    t_d.append([age, gender, genre])
+    t_d.append([age, gender.lower(), genre])
+    occurrences[gender.lower()][genre] = occurrences.get(genre, 0)+1
+    genres.add(genre)
 
 for row in test_data.iterrows():
     age, gender = row[1].values
 
-    d.append([age, gender])
+    d.append([age, gender.lower()])
 
-for i in t_d:
-    _, gender, genre = i
-    gender = gender.lower()
-
-    if genre not in classifying[gender]:
-        ages = [k_age for k_age, k_gender, k_genre in t_d if k_gender.lower() == gender and k_genre == genre]
-
-        classifying[gender][genre] = range(min(ages),max(ages)+1)
+for genre in genres:
+    for gender in ["male", "female"]:
+        ages = [k_age for k_age, k_gender, k_genre in t_d if k_gender == gender and k_genre == genre]
+        
+        if ages:
+            classifying[gender][genre] = range(min(ages),max(ages)+1)
 
 for i in d:
     age, gender = i
-    gender = gender.lower()
     possibilities = [genre for genre in classifying[gender]if age in classifying[gender][genre]]
 
-    for k in possibilities[1:]:
-        min_a, min_b = classifying[gender][possibilities[0]][0], classifying[gender][k][0]
-        differences = [age-min_a, age-min_b]
+    for k in possibilities[1:]: # comparing each possibility with the first possibility following the below rules
+        min_a, min_b, max_a, max_b = classifying[gender][possibilities[0]][0], classifying[gender][k][0], classifying[gender][possibilities[0]][1], classifying[gender][k][1]
+        differences = [abs(age-min_a), abs(age-min_b), abs(age-max_a), abs(age-max_b)]
+        occ_a = occurrences[gender][possibilities[0]]
+        occ_b = occurrences[gender][k]
+        
+        if differences[0] != differences[1]: # If the differences are not the same, look for the farthest one and remove it 
+            possibilities.remove([possibilities[0],k][max(differences[0:2])==differences[1]])
+        elif occ_a == occ_b: # if occurrences of a == occurrences of b look for the fathest one from the maximum and remove it
+            possibilities.remove([possibilities[0],k][max(differences[3:5])==differences[3]])
+        else: # otherwise, look for the least occurring genre and remove it
+            possibilities.remove([possibilities[0],k][min(occ_a, occ_b)==occ_b])
 
-        if differences[0] != differences[1]:
-            possibilities.remove([possibilities[0],k][max(differences)==age-min_b])
-        else: 
-            occurrences_a = sum([1 for _, k_gender, k_genre in t_d if k_gender == gender and k_genre == possibilities[0]])
-            occurrences_b = sum([1 for _, k_gender, k_genre in t_d if k_gender == gender and k_genre == k])
-            
-            possibilities.remove([possibilities[0],k][min(occurrences_a, occurrences_b)==occurrences_b])
-    
     print(f"{age} {gender} : {'/'.join(possibilities)}" if possibilities else None)
-
-
-input("\n> Exit")
